561744b469854c55d8f22bd21dc24102e534da34
k-electron <7101641+k-electron@users.noreply.github.com>
2022-03-10 22:52:19 -0500

    Remove unnecessary objects. Remove division to prevent float imprecision and divide-by-zero errors.

--- source/Patches/CrewmateRoles/ProphetMod/Revelation.cs
+++ source/Patches/CrewmateRoles/ProphetMod/Revelation.cs
@@ -7,29 +7,25 @@ namespace TownOfUs.CrewmateRoles.ProphetMod
     [HarmonyPatch(typeof(PlayerControl), nameof(PlayerControl.CompleteTask))]
     public class Revelation
     {
-        public static void Postfix(PlayerControl __instance)
+        public static void Postfix()
         {
             if (!PlayerControl.LocalPlayer.Data.IsDead
                 && PlayerControl.LocalPlayer.Is(RoleEnum.Prophet)
-                && ShouldReveal(__instance))
+                && ShouldReveal())
             {
                 Role.GetRole<Prophet>(PlayerControl.LocalPlayer).Revelation();
             }
         }
 
-        private static bool ShouldReveal(PlayerControl __instance)
+        private static bool ShouldReveal()
         {
-            var taskInfos = __instance.Data.Tasks.ToArray();
+            var taskInfos = PlayerControl.LocalPlayer.Data.Tasks.ToArray();
             var allTasksCount = taskInfos.Count;
-            var maxRevealsCount = 
-                CustomGameOptions.ProphetTotalReveals < 1 ? 1 : CustomGameOptions.ProphetTotalReveals;
+            var maxRevealsCount = CustomGameOptions.ProphetTotalReveals;
             var currentRevealsCount = Role.GetRole<Prophet>(PlayerControl.LocalPlayer).Revealed.Count;
 
-            var requiredTasksForNextReveal = (currentRevealsCount + 1)
-                                             * ((float) allTasksCount / maxRevealsCount);
-
             var completedTasksCount = taskInfos.Count(x => x.Complete);
-            return completedTasksCount >= requiredTasksForNextReveal;
+            return completedTasksCount * maxRevealsCount >= allTasksCount * (currentRevealsCount + 1);
         }
     }
 }
